d879415ac11e31d21976179b6a8f728e36312251
shuntagami <shuntagami23@gmail.com>
2022-12-16 12:31:28 +0900

    tweak puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -1,12 +1,14 @@
-workers ENV['SERVER_WORKER_COUNT'] || 2
+workers ENV.fetch('SERVER_WORKER_COUNT', 2)
 
-threads_count = ENV['SERVER_MAX_THREADS_COUNT'] || 5
-threads threads_count, threads_count
+max_threads_count = ENV.fetch('SERVER_MAT_THREADS_COUNT', 5)
+min_threads_count = ENV.fetch('SERVER_MIN_THREADS_COUNT', max_threads_count)
+threads min_threads_count, max_threads_count
 
 preload_app!
 
-port ENV['SERVER_PORT'] || 3000
-environment ENV['RACK_ENV'] || 'development'
+port ENV.fetch('SERVER_PORT', 3000)
+environment ENV.fetch('RACK_ENV', 'development')
+pidfile ENV.fetch('PIDFILE', 'tmp/pids/server.pid')
 
 on_worker_boot do
   Sequel.connect(ENV['DATABASE_URL'])
