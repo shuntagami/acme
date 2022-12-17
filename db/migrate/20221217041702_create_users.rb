de6af57acf93b1154275ab4f6029f55faa9b97d9
shuntagami <shuntagami23@gmail.com>
2022-12-18 01:47:54 +0900

    make db.create && make db.migrate

--- db/migrate/20221217041702_create_users.rb
+++ db/migrate/20221217041702_create_users.rb
@@ -4,6 +4,8 @@ class CreateUsers < ActiveRecord::Migration[7.0]
       t.unsigned_integer :id, primary_key: true, auto_increment: true, null: false
       t.string :access_token, null: false, unique: true
       t.string :name, null: false
+
+      t.timestamps
     end
     add_index :users, :access_token, unique: true
   end
