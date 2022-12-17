47fe85173444b4a1b5a1d32ae03eebcced0aa1b9
shuntagami <shuntagami23@gmail.com>
2022-12-17 17:47:07 +0900

    config.filter_run_when_matching :focus for RSpec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,8 @@ require "committee_helper"
 require "committee"
 
 RSpec.configure do |config|
+  config.filter_run_when_matching :focus
+
   config.include Rack::Test::Methods
   config.include Committee::Test::Methods
   config.include CommitteeHelper
