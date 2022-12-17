60d4354c73c4ef43e2d5939393f7dfd459ab3d9c
shuntagami <shuntagami23@gmail.com>
2022-12-18 01:42:30 +0900

    Support ja translation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,20 +9,20 @@ describe User, type: :model do
     it "is invalid if name is nil" do
       user.name = nil
       expect(user).to_not be_valid
-      expect(user.errors[:name]).to include("can't be blank")
+      expect(user.errors[:name]).to include("を入力してください")
     end
 
     it "is invalid if name is too long" do
       user.name = "a" * 21
       expect(user).to_not be_valid
-      expect(user.errors[:name]).to include("is too long (maximum is 20 characters)")
+      expect(user.errors[:name]).to include("は20文字以内で入力してください")
     end
 
     it "is invalid if access_token is duplicated" do
       User.create(name: "hoge", access_token: "token")
       user2 = User.create(name: "fuga", access_token: "token")
       expect(user2).to_not be_valid
-      expect(user2.errors[:access_token]).to include("has already been taken")
+      expect(user2.errors[:access_token]).to include("はすでに存在します")
     end
   end
 end
